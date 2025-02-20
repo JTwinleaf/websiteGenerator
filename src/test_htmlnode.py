4dfad16b3c0c111398ce4280af368f01fd16924f
JTwinleaf <jongarnett1996@gmail.com>
2025-02-20 15:42:43 -0600

    fixing html tester

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,7 +1,21 @@
+import unittest
 from htmlnode import *
 
-def main():
-    test_node = HTMLNode(props={"href": "https://www.twinleaf.town", "target": "my ass",})
-    print(test_node.props_to_html())
 
-main()
+class TestHTMLNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+    
+    def test_noteq(self):
+        node = TextNode("This is a miror", TextType.BOLD)
+        node2 = TextNode("You are a typo", TextType.NORMAL)
+        self.assertNotEqual(node, node2)
+    
+    def test_isinstance(self):
+        node = TextNode("I am not a TextNode", TextType.NORMAL)
+        self.assertIsNone(node.url)
+
+if __name__ == "__main__":
+    unittest.main()
