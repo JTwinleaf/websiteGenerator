8dd35fbaf20c562666b3f14efb249e9079cc7567
JTwinleaf <jongarnett1996@gmail.com>
2025-02-24 23:22:10 -0600

    Whoops, forgot to push these before

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -5,18 +5,31 @@ class HTMLNode:
         self.value = value
         self.children = children
         self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, [], props)
     
     def to_html(self):
-        raise NotImplementedError("Invalid")
-    
-    def props_to_html(self):
-        props = self.props
-        output = ""
-        if props == None:
-            return
-        for key in props:
-            output += f' {key}="{props[key]}"'
-        return output
+        if self.value is None:
+            raise ValueError("Invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
     
     def __repr__(self):
-        return f"HTMLNote({self.text}, {self.text_type.value}, {self.url})"
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
