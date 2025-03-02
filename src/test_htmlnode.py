369982cd313c92a917f1a57986cd127fd97c290a
JTwinleaf <jongarnett1996@gmail.com>
2025-03-02 13:00:40 -0600

    no real changes since last commit, just added a blank method for testing parent to_html

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -56,6 +56,9 @@ class TestHTMLNode(unittest.TestCase):
     def test_to_html_no_tag(self):
         node = LeafNode(None, "Hello, world!")
         self.assertEqual(node.to_html(), "Hello, world!")
+    
+    def test_to_html_parent(self):
+        pass
 
 
 if __name__ == "__main__":
