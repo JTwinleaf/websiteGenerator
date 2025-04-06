a232a434d752d98ca098b95655be76472e342235
JTwinleaf <jongarnett1996@gmail.com>
2025-04-06 10:47:31 -0500

    I AM BACK!

--- src/markdown_to_textnode.py
+++ src/markdown_to_textnode.py
@@ -1,4 +1,35 @@
-
+from textnode import *
+from htmlnode import *
+from txt_to_html import *
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    pass
+    new_nodes = []
+    inside_delimiter = False
+    formatted_text = ""
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        
+    parts = node.text.split(delimiter)
+
+    for i, part in enumerate(parts):
+        if inside_delimiter:
+            formatted_text += part
+
+            if i < len(parts) - 1:
+                new_nodes.append(TextNode(formatted_text, text_type))
+                formatted_text = ""
+                inside_delimiter = False
+        
+        else:
+            new_nodes.append(TextNode(part, TextType.TEXT))
+
+            if i < len(parts) - 1:
+                inside_delimiter = True
+
+    if inside_delimiter:
+        raise ValueError("Unclosed delimiter found in text node.")
+
+    return new_nodes   
