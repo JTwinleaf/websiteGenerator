a9c5a2f9fdd5ffdec385f8b62fc18ecc7c2ed488
JTwinleaf <jongarnett1996@gmail.com>
2025-04-07 09:50:05 -0500

    forgot to push this yesterday

--- src/markdown_to_textnode.py
+++ src/markdown_to_textnode.py
@@ -12,22 +12,22 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(node)
             continue
         
-    parts = node.text.split(delimiter)
+        parts = node.text.split(delimiter)
 
-    for i, part in enumerate(parts):
-        if inside_delimiter:
-            formatted_text += part
+        for i, part in enumerate(parts):
+            if inside_delimiter:
+                formatted_text += part
 
-            if i < len(parts) - 1:
-                new_nodes.append(TextNode(formatted_text, text_type))
-                formatted_text = ""
-                inside_delimiter = False
-        
-        else:
-            new_nodes.append(TextNode(part, TextType.TEXT))
+                if i < len(parts) - 1:
+                    new_nodes.append(TextNode(formatted_text, text_type))
+                    formatted_text = ""
+                    inside_delimiter = False
+            
+            else:
+                new_nodes.append(TextNode(part, TextType.TEXT))
 
-            if i < len(parts) - 1:
-                inside_delimiter = True
+                if i < len(parts) - 1:
+                    inside_delimiter = True
 
     if inside_delimiter:
         raise ValueError("Unclosed delimiter found in text node.")
